0c86315f4fcb03b832fca4a38ede6b3118ca429f
chesiren <44122832+chesiren@users.noreply.github.com>
2025-12-26 12:13:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,52 @@
 import { WebSocketServer } from 'ws';
+import crypto from 'crypto';
+
 
 const wss = new WebSocketServer({ port: 8080 });
 console.log("Websocket opened on port 8080")
 
 var connections = {};
+var inputs = {};
 
 wss.on('connection', function connection(ws, req)
 {
-    console.log('Client connected');
-    console.log(req.headers);
-
-    ws.id = req.headers['sec-websocket-key'];
+    ws.id = crypto.randomUUID();
     ws.io = req.headers['cc-io'];
     ws.key = req.headers['cc-key'];
 
     connections[ws.id] = ws;
 
+    console.log('Client connected:', ws.io, ws.key);
+
+    if (ws.io === "input")
+    {
+        inputs[ws.key] = ws;
+    }
+
     ws.on('message', function incoming(message)
     {
-        if (ws.io == "input")
+        if (ws.io === "input")
         {
-            console.log(ws.id + ' : Received microphone data:', message);
- 
             for (let id in connections)
             {
-                if (connections[id] && connections[id].io == "output" && connections[id].key == ws.key)
+                const c = connections[id];
+                if (c && c.io === "output" && c.key === ws.key)
                 {
-                    connections[id].send(message);
+                    c.send(message);
                 }
             }
         }
     });
 
-    ws.on('close', function close()
+    ws.on('close', function ()
     {
-        console.log('Client disconnected');
-        //connections[ws.id] = null;
+        console.log('Client disconnected:', ws.io, ws.key);
+
         delete connections[ws.id];
+
+        if (inputs[ws.key] === ws)
+        {
+            delete inputs[ws.key];
+        }
     });
 });
